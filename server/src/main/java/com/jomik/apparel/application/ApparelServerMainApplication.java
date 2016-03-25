d535c555d455cc1843946bc464e5ccb87857ce5b
MiyagiMick <mickoallen@gmail.com>
2016-03-25 15:22:24 -0400

    add spring boot-ness

---
+++ server/src/main/java/com/jomik/apparel/application/ApparelServerMainApplication.java
@@ -0,0 +1,16 @@
+package com.jomik.apparel.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mick on 25/03/2016.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ApparelServerMainApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApparelServerMainApplication.class, args);
+    }
+}
