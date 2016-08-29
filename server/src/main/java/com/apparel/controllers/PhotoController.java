918040173f3965c9f9889be2803dcc4adc6f4afa
joeydeluca <joeydeluca@hotmail.com>
2016-08-28 22:44:49 -0400

    sync

--- server/src/main/java/com/apparel/controllers/PhotoController.java
+++ server/src/main/java/com/apparel/controllers/PhotoController.java
@@ -5,10 +5,7 @@ import com.apparel.domain.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.websocket.server.PathParam;
@@ -38,7 +35,7 @@ public class PhotoController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/photos/{uuid}")
-    public ResponseEntity<Void> handleFileUpload(@PathParam("uuid") String uuid, @RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<Void> handleFileUpload(@PathVariable("uuid") String uuid, @RequestParam("file") MultipartFile file) throws IOException {
 
         Photo photo = photoRepository.findOne(uuid);
         photo.setThumbnail(file.getBytes());
